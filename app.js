2f086ceb119a3ae297e7beb6107254d161e674d2
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-02-07 19:11:47 -0800

    added some jquery to the sitegit add . wooooo I'm a master

--- app.js
+++ app.js
@@ -7,6 +7,9 @@
 var userNameForm = document.getElementById('username-form');
 var welcomeMessage = document.getElementById('welcome-message');
 
+
+
+
 // function for userName form submittal
 function displayUserName(e) {
   e.preventDefault();
@@ -17,10 +20,19 @@ function displayUserName(e) {
   localStorage.setItem('localStorageUserName', JSON.stringify(userNameVariable));
 
   welcomeMessage.textContent = 'welcome, ' + userNameVariable;
-
 }
 
 
 
 // add event listener for userName form submittal
 userNameForm.addEventListener('submit', displayUserName);
+
+
+// jQuery... ?
+$(document).ready(function(){
+  $('#directions-for-user').hide();
+  $('#username-form button').click(function() {
+    $('#username-form').fadeOut('slow');
+    $('#directions-for-user').delay(500).fadeIn('slow');
+  });
+});
